40f4aa5c94c33ef3f37e29719b17151bbd1e8bec
azzy <arianne@hey.com>
2023-02-05 21:16:28 +0000

    adding new calculator files

--- Exercise10/pin_number.py
+++ Exercise10/pin_number.py
@@ -1,22 +1,27 @@
 import getpass
 
+# set intro variables
 our_pin = "1234"
 attempts = 0
 allowed_attempts = 3
 asking_for_input = True
 
-
+# while loop asking for input, as long as asking_for_input is true
 while asking_for_input:
     supplied_pin = getpass.getpass(prompt="Enter your PIN: ")
 
+    # if the entered pin matches, print message and alter loop condition
     if supplied_pin == our_pin:
         print("Thank you for entering your PIN")
         print("Welcome to the Bank. How can we help you today?")
         asking_for_input = False
+
+    # otherwise add one to the attempts counter and prompt for more input
     else:
         attempts += 1
         print("Invalid PIN entered. You have " + str(allowed_attempts-attempts) + " attempts remaining.")
 
+    # if attempts counter equals the number of allowed attempts, then alter loop condition and print message
     if attempts == allowed_attempts:
         asking_for_input = False
         print("You've exhausted your PIN attempts. Please try again later")
--- Exercise9/calculator 2.py
+++ Exercise9/calculator 2.py
@@ -1,9 +1,21 @@
-firstNumber = input("Please enter a numeric value: ")
-secondNumber = input("Please enter a numeric value: ")
+# ask the user for input
+number1 = input("Please enter the first number you would like to add: ")
+number2 = input("Please enter the second number you would like to add: ")
+operator = input("Please select the operator to use. Choose from +, -, *, or /: ")
 
-if firstNumber.isdecimal() == False or secondNumber.isdecimal() == False:
-    print("Please only use numeric values")
+# check which operator was used and calculate accordingly
+if operator == "+":
+    print("{} {} {} = {}".format(number1, operator, number2, float(number1) + float(number2)))
 
+elif operator == "-":
+    print("{} {} {} = {}".format(number1, operator, number2, float(number1) - float(number2)))
+
+elif operator == "*":
+    print("{} {} {} = {}".format(number1, operator, number2, float(number1) * float(number2)))
+
+elif operator == "/":
+    print("{} {} {} = {}".format(number1, operator, number2, float(number1) / float(number2)))
+
+# if an incorrect operator was used, flag with user and remind of accepted inputs
 else:
-    print(float(firstNumber) + float(secondNumber))
-
+    print("Please enter a valid operator. Only +, -. *, or / are permitted")
