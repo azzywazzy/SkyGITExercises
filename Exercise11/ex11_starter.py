c779915ad5df5e44d92e6941a72786b300838c8c
azzy <arianne@hey.com>
2023-02-11 19:08:34 +0000

    adding exercise11

--- Exercise11/ex11_starter.py
+++ Exercise11/ex11_starter.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+belgianList = Belgium.split(",")
 
+print("-" * len(Belgium))
+
+print(Belgium.replace(",",":"))
+
+print("The combined population of Belgium and Brussels is {}".format((int(belgianList[1]) + int(belgianList[3]))))
